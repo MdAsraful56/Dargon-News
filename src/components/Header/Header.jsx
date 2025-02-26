769f33a88ccf4b65959a90c5a990f57bb084ed2d
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-26 22:40:37 +0600

    monent.js addIng time formate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import logo from '../../assets/logo.png';
 import Navbar from '../Navbar/Navbar';
 
@@ -8,10 +9,16 @@ const Header = () => {
             <header className='flex flex-col items-center justify-center gap-2'>
                 <img className='w-[350px]' src={logo} alt="" />
                 <h4 className="text-lg text-gray-400">Journalism Without Fear or Favour</h4>
+                <p className="text-base font-semibold text-gray-300">
+                    {
+                        moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
+                    }
+                </p>
                 <div className="flex flex-row items-center justify-center gap-2">
                     <button className="btn btn-error text-white text-lg">Latest</button>
                     <marquee className='text-lg'>Match Highlights: Germany vs Spain — as it happened ! Match Highlights: Germany vs Spain as...</marquee>
                 </div>
+                
                 <Navbar />
             </header>
         </div>
