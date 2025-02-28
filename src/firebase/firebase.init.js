10e026bed9ce7193a4ed8d1c82ea13c873e6b1ef
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-28 09:29:22 +0600

    firebase install & setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB64JSy94cHE89yZHFZIraK7_tRzr2Fm6A",
+  authDomain: "dargon-news-37f86.firebaseapp.com",
+  projectId: "dargon-news-37f86",
+  storageBucket: "dargon-news-37f86.firebasestorage.app",
+  messagingSenderId: "486855030194",
+  appId: "1:486855030194:web:adbfe1851684d86d4e20ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
