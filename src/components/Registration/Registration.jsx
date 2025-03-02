d338cf7c4aa08f1d25cf92388f2b8f3b07f800a1
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-02 06:23:29 +0600

    registration page design

---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,50 @@
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+
+
+
+const Registration = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log('login');
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
+
+
+    return (
+        <div className="login bg-gray-700 p-10 w-1/2 mx-auto rounded-lg mt-10 ">
+            <h1 className="text-3xl mb-8">Registration new account</h1>
+            <div className="text-left p-10 bg-gray-800 rounded-lg">
+                <form onSubmit={handleLogin}>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-white">Email</label>
+                        <input type="email" name="email" id="email" className="w-full p-2 rounded-lg" />
+                    </div>
+                    <div className="mb-4 relative">
+                        <label htmlFor="password" className="block text-white">Password</label>
+                        <input type={showPassword ? 'text' : 'password'} name="password" id="password" className="w-full p-2 rounded-lg" />
+                        {/* {
+                            showPassword ? <FaEyeSlash className="absolute right-4 top-9 text-white" /> : <FaEye className="absolute right-4 top-9 text-white" />
+                        } */}
+                        <button onClick={() => {setShowPassword(!showPassword)}}  type="button">
+                            {
+                                showPassword ?  <FaEye className="absolute right-4 top-9 text-white" /> : <FaEyeSlash className="absolute right-4 top-9 text-white" />
+                            }
+                        </button>
+                    </div>
+                    <button className="btn btn-primary w-full">Registration</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Registration;
